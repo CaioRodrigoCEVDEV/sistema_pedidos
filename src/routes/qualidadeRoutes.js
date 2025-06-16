be744103d34971bcefaf1c05892c7afaade78fc4
Caio Rodrigo <contato@caiorodrigocev.com.br>
2025-06-16 18:16:07 -0300

    removido tabela de qualiade

--- src/routes/qualidadeRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const qualidadeController = require('../controllers/qualidadeController');
-//const autenticarToken = require('../src/middleware/authMiddleware');  // desatvado por enquanto 
-
-router.get('/qualidade', qualidadeController.listarQualidade);
-
-module.exports = router;
