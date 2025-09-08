3302588e0365aab2606337c11fb190c865074335
Heitor Gonçalves Barbosa <heitorpet1@gmail.com>
2025-09-08 10:59:37 -0300

    criado função para gerar mensagem de cobrança criado novos campos na tabela emp datapag e datavenc

--- src/routes/empRoutes.js
+++ src/routes/empRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const empController = require("../controllers/empController");
-const autenticarToken = require('../middlewares/middlewares');
+const autenticarToken = require("../middlewares/middlewares");
 
 router.get("/emp", empController.listarEmpresa);
-router.put("/emp",autenticarToken, empController.editarNumeroEmpresa);
-
+router.get("/emp/pagamento", autenticarToken, empController.dadosPagamento);
+router.put("/emp", autenticarToken, empController.editarNumeroEmpresa);
 
 module.exports = router;
