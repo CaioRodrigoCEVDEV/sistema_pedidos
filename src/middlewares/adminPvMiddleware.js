735fc91b3d4cfe87dcb2146aa12bb14f841308b8
Caio Rodrigo <contato@caiorodrigocev.com.br>
2025-10-28 21:52:39 -0300

    FEAT: Implementado validação Backend para ESTOQUE/PEDIDO

---
+++ src/middlewares/adminPvMiddleware.js
@@ -0,0 +1,54 @@
+const jwt = require('jsonwebtoken');
+
+function requireAdminPv(req, res, next) {
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.status(401).redirect('/login');
+    }
+
+    try {
+        const decoded = jwt.verify(token, 'chave-secreta');
+        // gera de novo um novo token com 10 minutos
+        const novoToken = jwt.sign({
+            usuemail: decoded.usuemail,
+            usucod: decoded.usucod,
+            usunome: decoded.usunome,
+            usuadm: decoded.usuadm,
+            usupv: decoded.usupv
+        }, 'chave-secreta', { expiresIn: '60m' });
+
+        // gauda o novo token com mais 10m em cookies
+        res.cookie('token', novoToken, {
+            httpOnly: true,
+            sameSite: 'Strict',
+            secure: process.env.HTTPS,
+        });
+        // Remove os outros cookies inseguros, se ainda existirem
+        res.clearCookie('usucod');
+        res.clearCookie('usunome');
+        res.clearCookie('usuemail');
+        res.clearCookie('usuadm');
+        res.clearCookie('usupv');
+
+        req.token = decoded; // Armazena dados decodificados para uso futuro
+
+        if (decoded && decoded.usupv === 'S') {
+            req.user = decoded; // Optional: store decoded user info
+            next();
+        } else {
+            //return res.status(403).redirect('/painel');
+            return res.status(403).send(`
+                <script>
+                    alert("⚠️ Acesso negado: você não tem permissão para acessar esta página.");
+                    window.location.href = "/painel";
+                </script>
+                `);
+
+        }
+    } catch (err) {
+        return res.status(500).redirect('/login');
+    }
+}
+
+module.exports = requireAdminPv;
