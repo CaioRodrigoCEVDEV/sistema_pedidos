ef144a87487beb982645cb5f10d6b916594834fa
Caio Rodrigo <contato@caiorodrigocev.com.br>
2025-11-28 18:45:40 -0300

    Refactor: incluir middleware de autenticação em rota pública /v2/pedidos/

--- src/routes/pedidosRoutesV2.js
+++ src/routes/pedidosRoutesV2.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const pedidosControllerV2 = require("../controllers/pedidosControllerV2");
-//const autenticarToken = require("../middlewares/middlewares");
+const autenticarToken = require("../middlewares/middlewares");
 
-router.get("/v2/pedidos/total/anual", pedidosControllerV2.totalVendasAnual);
-router.get("/v2/pedidos/total/dia", pedidosControllerV2.totalVendasDia);
-router.get("/v2/pedidos/total", pedidosControllerV2.totalVendas);
-router.get("/v2/pedidos/listar", pedidosControllerV2.listarPv);
-router.put("/v2/pedidos/cancelar/:pvcod", pedidosControllerV2.cancelarPedido);
+router.get("/v2/pedidos/total/anual",autenticarToken, pedidosControllerV2.totalVendasAnual);
+router.get("/v2/pedidos/total/dia",autenticarToken, pedidosControllerV2.totalVendasDia);
+router.get("/v2/pedidos/total",autenticarToken, pedidosControllerV2.totalVendas);
+router.get("/v2/pedidos/listar",autenticarToken, pedidosControllerV2.listarPv);
+router.put("/v2/pedidos/cancelar/:pvcod", autenticarToken,pedidosControllerV2.cancelarPedido);
 
 module.exports = router;
